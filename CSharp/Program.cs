29f39b2b67c7e249bb558e4c260f8cde3565a79f
axel <axdavax93@gmail.com>
2026-02-08 16:49:45 -0500

    Work in progress: update Program.cs

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,14 +1,12 @@
-﻿using LeetCore.CSharp.Problems.Easy;
+﻿using LeetCode.CSharp.Problems.Easy;
 
-namespace CSharp
+namespace LeetCode.CSharp;
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // You can test problems by calling their Test() method here.
-            // For example:
-            // P1207_Unique_Number_Of_Occurences.Test();        
-        }
+        // You can test problems by calling their Test() method here.
+        // For example:
+        // P1207_Unique_Number_Of_Occurences.Test();        
     }
 }
