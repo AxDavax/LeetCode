928cf28c2be4a138852a8342b23077e38fa21023
AxDavax <axdavax93@gmail.com>
2026-02-08 11:53:11 -0500

    Refactor namespace and using directives in Program.cs

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,14 +1,13 @@
-﻿using LeetCore.CSharp.Problems.Easy;
+﻿using LeetCode.CSharp.Problems.Easy;
 
-namespace CSharp
+namespace LeetCode.CSharp;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // You can test problems by calling their Test() method here.
-            // For example:
-            // P1207_Unique_Number_Of_Occurences.Test();        
-        }
+        // You can test problems by calling their Test() method here.
+        // For example:
+        // P1207_Unique_Number_Of_Occurences.Test();        
     }
 }
