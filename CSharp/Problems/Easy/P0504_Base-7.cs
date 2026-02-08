817d9e83c4888e4ae640315ca2e052ce15bef905
axel <axdavax93@gmail.com>
2026-02-08 17:35:21 -0500

    Refactor P0504 Base 7: Solution + Test

---
+++ CSharp/Problems/Easy/P0504_Base-7.cs
@@ -0,0 +1,71 @@
+﻿/*
+    504. Base 7
+    
+    Given an integer num, return a string of its base 7 representation.
+
+ 
+    Example 1:
+
+        Input: num = 100
+        Output: "202"
+
+    Example 2:
+
+        Input: num = -7
+        Output: "-10"
+
+
+    Constraints:
+
+        -10^7 <= num <= 10^7
+*/
+
+using System.Text;
+
+namespace LeetCode.CSharp.Problems.Easy;
+
+public class P0504_Base_7
+{
+    #region Solution
+
+    /// <summary>
+    /// Complexité temporelle : O(log7(n))
+    /// Complexité spatialle : O(log7(n))
+    /// </summary>
+    public static string ConvertToBase7(int num)
+    {
+        if (num == 0) return "0";
+        
+        var res = new StringBuilder();
+        
+        int n = Math.Abs(num);
+        while (n > 0)
+        {
+            res.Append(n % 7);
+            n /= 7;
+        }
+        
+        if (num < 0) res.Append('-');
+        char[] arr = res.ToString().ToCharArray();
+        
+        Array.Reverse(arr);
+        return new string(arr);
+    }
+
+    #endregion
+
+    #region Test
+
+    public static void Test()
+    {
+        int num = 100;
+        Console.WriteLine($"Input: num = {num}");
+        Console.WriteLine($"Output: \"{ConvertToBase7(num)}\"");
+
+        num = -7;
+        Console.WriteLine($"Input: num = {num}");
+        Console.WriteLine($"Output: \"{ConvertToBase7(num)}\"");
+    }
+
+    #endregion
+}
